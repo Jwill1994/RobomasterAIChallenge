e0ed744f9dcde63c6ab29bd3db9f6d1d5c2e2864
ByunJoowoong <woong164@naver.com>
2019-03-08 04:15:16 +0900

    update roborts

--- Ros_old/roborts/roborts_shooter/src/roborts_shooter_plugin.cc
+++ Ros_old/roborts/roborts_shooter/src/roborts_shooter_plugin.cc
@@ -30,6 +30,7 @@
 #include <ignition/math/Pose3.hh>
 #include <ignition/math/Vector3.hh>
 #include <ignition/math/Quaternion.hh>
+#include <ignition/math/Matrix3.hh>
 
 namespace gazebo
 {
@@ -44,7 +45,9 @@ namespace gazebo
         {
             this->model_ = _parent;
             this->gimbal_link = _sdf->Get<std::string>("gimbal_link");
+            this->fake_link = _sdf->Get<std::string>("fake_link");
             this->reference_frame_ = _sdf->Get<std::string>("reference_frame");
+            this->force = _sdf->Get<double>("force");
             
             // Initialize ros, if it has not already bee initialized.
             if (!ros::isInitialized())
@@ -62,8 +65,7 @@ namespace gazebo
             //ros service
             this->gazebo_ros_link_state = this->ros_nh_.serviceClient<gazebo_msgs::GetLinkState>("/gazebo/get_link_state");
             
-            srv.request.link_name = this->gimbal_link;
-            srv.request.reference_frame = this->reference_frame_;
+            
 
             
 
@@ -161,6 +163,35 @@ namespace gazebo
         {
 
 
+            srv_.request.link_name = this->fake_link;
+            srv_.request.reference_frame = this->reference_frame_;
+
+            if(gazebo_ros_link_state.call(srv_)  && srv.response.success){
+                // ROS_WARN("%d", srv_.response.success);
+
+                // ROS_WARN("RESPONSE : %f, %f, %f", srv_.response.link_state.pose.position.x,srv_.response.link_state.pose.position.y,srv_.response.link_state.pose.position.z);
+                // ROS_WARN("GIMBAL_POSE : %f, %f, %f", this->gimbal_pose.position.x,this->gimbal_pose.position.y,this->gimbal_pose.position.z);
+
+                this->x_dir = srv_.response.link_state.pose.position.x - this->gimbal_pose.position.x;
+                this->y_dir = srv_.response.link_state.pose.position.y - this->gimbal_pose.position.y;
+                this->z_dir = srv_.response.link_state.pose.position.z - this->gimbal_pose.position.z;
+
+                // ROS_WARN("BEFORE NORMALIZE : X_DIR, Y_DIR, Z_DIR = %f, %f, %f", this->x_dir, this->y_dir, this->z_dir);
+
+                ignition::math::Vector3d vec = ignition::math::Vector3d(this->x_dir, this->y_dir, this->z_dir).Normalize();
+
+
+                this->x_dir = vec.X();
+                this->y_dir = vec.Y();
+                this->z_dir = vec.Z();
+
+                // ROS_WARN("AFTER NORMALIZE : X_DIR, Y_DIR, Z_DIR = %f, %f, %f", this->x_dir, this->y_dir, this->z_dir);
+            }
+
+
+            srv.request.link_name = this->gimbal_link;
+            srv.request.reference_frame = this->reference_frame_;
+
             if(gazebo_ros_link_state.call(srv) && srv.response.success){
                 this->gimbal_pose.position.x = srv.response.link_state.pose.position.x;
                 this->gimbal_pose.position.y = srv.response.link_state.pose.position.y;
@@ -171,43 +202,72 @@ namespace gazebo
                 this->gimbal_pose.orientation.z = srv.response.link_state.pose.orientation.z;
                 this->gimbal_pose.orientation.w = srv.response.link_state.pose.orientation.w;
 
+
+
+                // this->q_ = new ignition::math::Quaternion<double>( \
+                //     this->gimbal_pose.orientation.w, this->gimbal_pose.orientation.x, \
+                //     this->gimbal_pose.orientation.y, this->gimbal_pose.orientation.z \
+                // );
+                // // q_.qx = this->gimbal_pose.orientation.x;
+                // // q_.qy = this->gimbal_pose.orientation.y;
+                // // q_.qz = this->gimbal_pose.orientation.z;
+                // // q_.qw = this->gimbal_pose.orientation.w;
+
+                // if(this->gimbal_pose.orientation.w > 1) {this->q_->Normalize();}
+                // double angle = 2 * std::acos(this->gimbal_pose.orientation.w);
+                // double s = std::sqrt(1 - std::pow(this->gimbal_pose.orientation.w,2));
+                // if(s < 0.001) {
+                //     this->x_dir = this->gimbal_pose.orientation.x;
+                //     this->y_dir = this->gimbal_pose.orientation.y;
+                //     this->z_dir = this->gimbal_pose.orientation.z;
+                // } else {
+                //     this->x_dir = this->gimbal_pose.orientation.x / s;
+                //     this->y_dir = this->gimbal_pose.orientation.y / s;
+                //     this->z_dir = this->gimbal_pose.orientation.z / s;
+                // }
+
+                // this->gimbal_roll = this->q_->Euler().X();
+                // this->gimbal_pitch = this->q_->Euler().Y();
+                // this->gimbal_yaw = this->q_->Euler().Z();
+
+                
+
                 // ROS_INFO("%f", this->gimbal_pose.position.x);
                 // ROS_INFO("%f", this->gimbal_pose.position.y);
                 // ROS_INFO("%f", this->gimbal_pose.position.z);
             }
 
-        //     if (this->reseting_plugin)
-        //     {
-        //         ROS_ERROR("Reseting in Process, please wait...");
-        //     } else
-        //     {
-        //         // TODO: Check what is necessary now here
-        //         double new_secs =this->world->GetSimTime().Float();
-        //         double delta = new_secs - this->old_secs;
 
-        //         double max_delta = 0.0;
 
-        //         if (this->reset_frequency != 0.0)
-        //         {
-        //         max_delta = 1.0 / this->reset_frequency;
-        //         }
 
-        //         if (delta > max_delta && delta != 0.0)
-        //         {
-        //             // We update the Old Time variable.
-        //             this->old_secs = new_secs;
-                    
-                    
-        //             if (this->model_to_update_index_now >= this->modelIDToName_size)
-        //             {
-        //                 this->model_to_update_index_now = 0;
-        //             }
+
+            // TODO: Check what is necessary now here
+            double new_secs =this->model_->GetWorld()->GetSimTime().Float();
+            double delta = new_secs - this->old_secs;
+
+            double max_delta = 0.0;
+
+            if (this->reset_frequency != 0.0)
+            {
+            max_delta = 1.0 / this->reset_frequency;
+            }
+
+            if (delta > max_delta && delta != 0.0)
+            {
+                // We update the Old Time variable.
+                this->old_secs = new_secs;
                 
-        //             // Update the Particles
-        //             UpdateParticles(this->model_to_update_index_now);
-        //             this->model_to_update_index_now ++;
-        //         }
-        //     }
+                
+                if (this->link_to_update_index_now >= this->linkIDToName_size)
+                {
+                    this->link_to_update_index_now = 0;
+                }
+            
+                // Update the Particles
+                UpdateBullets(this->link_to_update_index_now);
+                this->link_to_update_index_now ++;
+            }
+            
         }
 
         void WaitForseconds(float seconds_to_wait)
@@ -220,19 +280,22 @@ namespace gazebo
 
         }
 
-        // void UpdateParticles(int model_to_update_index)
-        // {
-        //     for (auto model : this->world->GetModels())
-        //     {
-        //         std::string model_name = model->GetName();
-        //         if (this->modelIDToName[model_to_update_index] == model_name)
-        //         {
-        //             this->MoveParticle(model);
-        //             this->SetForceParticle(model);
-        //         }
+        void UpdateBullets(int link_to_update_index_now)
+        {
+            for (auto model : this->model_->GetWorld()->GetModels())
+            {
+                std::string model_name = model->GetName();
+                if (this->linkIDToName[link_to_update_index_now] == model_name)
+                {
+                    if(link_to_update_index_now==2){
+                    this->MoveBullet(model);
+                    this->SetForceBullet(model);
+                    }
+                }
                 
-        //     }
-        // }
+            }
+            // delete this->q_;
+        }
         
         
         
@@ -243,31 +306,19 @@ namespace gazebo
             this->linkIDToName_size = 0;
             
             int i = 0;
-            for (auto link : this->model_->GetLinks())
+            for (auto model : this->model_->GetWorld()->GetModels())
             {
-                std::string link_name = link->GetName();
-                ROS_WARN("%s", link_name.c_str());
-                if (link_name.find("pitch_Link") != std::string::npos)
+                std::string model_name = model->GetName();
+                // ROS_WARN("%s", model_name.c_str());
+                if (model_name.find(this->bullet_link_name) != std::string::npos)
                 {
-                    ROS_WARN("hh");
-                    for(auto child_link : link->GetChildJointsLinks()) {
-                        std::string child_link_name = child_link->GetName();
-                        ROS_WARN("%s", child_link_name.c_str());
-                        if (child_link_name.find(this->bullet_link_name) != std::string::npos) {
-                            
-                            this->linkIDToName[i] = child_link->GetName();
-                            i ++;
-                        }
-                        
-                    }
-                    
+                    this->linkIDToName[i] = model->GetName();
+                    i ++;
                 }
             
             }
-            ROS_WARN("%s", this->model_->GetLink("bullet_0")->GetName().c_str());
             
             this->linkIDToName_size = linkIDToName.size();
-            ROS_WARN("linkIDToName Size : %d", this->linkIDToName_size);
         }
         
         void OutputBulletList()
@@ -284,56 +335,56 @@ namespace gazebo
         }
         
         
-        // void MoveParticle(boost::shared_ptr<gazebo::physics::Model> model)
-        // {
-
-        //     std::string model_name = model->GetName();
-
-        //     float x_pos_rand = 0.0;
-        //     float y_pos_rand = 0.0;
-        //     float z_pos_rand = 0.0;
-        //     float roll_rand = 0.0;
-        //     float pitch_rand = 0.0;
-        //     float yaw_rand = 0.0;
-
-        //     // If the model name contains the substring particle, we consider it a particle
-        //     if (model_name.find(this->particle_base_name) != std::string::npos)
-        //     {
-        //         ROS_WARN("Moving model=%s",model_name.c_str());
-
-        //         float x_pos_rand = RandomFloat(this->x_origin - this->random_range, this->x_origin + this->random_range);
-        //         float y_pos_rand = RandomFloat(this->y_origin - this->random_range, this->y_origin + this->random_range);
-        //         float z_pos_rand = RandomFloat(this->z_origin - this->random_range, this->z_origin + this->random_range);
-                
-        //         ROS_DEBUG("POSE-RANDOM[X,Y,Z,Roll,Pitch,Yaw=[%f,%f,%f,%f,%f,%f], model=%s", x_pos_rand,y_pos_rand,z_pos_rand,roll_rand,pitch_rand,yaw_rand,model_name.c_str());
-        //         //ignition::math::Pose3 initPose(ignition::math::Vector3<float>(x_pos_rand, y_pos_rand, z_pos_rand), ignition::math::Quaternion<float>(roll_rand, pitch_rand, yaw_rand));
-                
-        //         model->SetWorldPose(
-        //                             ignition::math::Pose3d(
-        //                                 ignition::math::Vector3d(x_pos_rand, y_pos_rand, z_pos_rand),
-        //                                 ignition::math::Quaterniond(roll_rand, pitch_rand, yaw_rand)
-        //                                                 )
-        //                             );
-                
-        //         ROS_DEBUG("Moving model=%s....END",model_name.c_str());
-
-        //     }
-
-            
-
-        // }
-        
-        
-        void SetForceBullet(boost::shared_ptr<gazebo::physics::Link> link)
+        void MoveBullet(boost::shared_ptr<gazebo::physics::Model> model)
         {
-            
-            std::string link_name = link->GetName();
+
+            std::string model_name = model->GetName();
 
             // If the model name contains the substring particle, we consider it a particle
-            if (link_name.find(this->bullet_link_name) != std::string::npos)
+            if (model_name.find(this->bullet_link_name) != std::string::npos)
             {
-                ROS_WARN("FORCE APPLIED[X,Y,Z]=[%f,%f,%f]", this->x_axis_force, this->y_axis_force, this->z_axis_force);
-                link->SetForce(ignition::math::Vector3d(this->x_axis_force, this->y_axis_force, this->z_axis_force));
+                ROS_WARN("Moving model=%s",model_name.c_str());
+                
+                ROS_WARN("GIMBAL-POSE[X,Y,Z,X_AXIS,Y_AXIS,Z_AXIS]=[%f,%f,%f,%f,%f,%f], model=%s", 
+                    this->gimbal_pose.position.x + this->gimbal_length * this->x_dir,
+                    this->gimbal_pose.position.y + this->gimbal_length * this->y_dir,
+                    this->gimbal_pose.position.z + this->gimbal_length * this->z_dir - 0.05, 
+                    this->x_dir,this->y_dir,this->z_dir, model_name.c_str());
+                //ignition::math::Pose3 initPose(ignition::math::Vector3<float>(x_pos_rand, y_pos_rand, z_pos_rand), ignition::math::Quaternion<float>(this->gimbal_roll, this->gimbal_pitch, this->gimbal_yaw));
+                
+                model->SetWorldPose(ignition::math::Pose3d(
+                                    ignition::math::Vector3d(this->gimbal_pose.position.x + this->gimbal_length * this->x_dir, 
+                                                             this->gimbal_pose.position.y + this->gimbal_length * this->y_dir, 
+                                                             this->gimbal_pose.position.z + this->gimbal_length * this->z_dir - 0.05),
+                                    ignition::math::Quaternion<double>(this->gimbal_pose.orientation.w, this->gimbal_pose.orientation.x,
+                                                            this->gimbal_pose.orientation.y, this->gimbal_pose.orientation.z))
+                                    );
+
+            }
+
+            
+
+        }
+
+        
+        void SetForceBullet(boost::shared_ptr<gazebo::physics::Model> model)
+        {
+            
+            std::string model_name = model->GetName();
+
+            double x_axis_force;
+            double y_axis_force;
+            double z_axis_force;
+
+            x_axis_force = this->x_dir * this->force;
+            y_axis_force = this->y_dir * this->force;
+            z_axis_force = this->z_dir * this->force;
+
+            // If the model name contains the substring particle, we consider it a particle
+            if (model_name.find(this->bullet_link_name) != std::string::npos)
+            {
+                ROS_WARN("FORCE APPLIED[X,Y,Z]=[%f,%f,%f]", x_axis_force, y_axis_force, z_axis_force);
+                model->GetLink("link")->SetForce(ignition::math::Vector3d(x_axis_force, y_axis_force, z_axis_force));
             }
         }
 
@@ -401,11 +452,16 @@ namespace gazebo
             ros::ServiceServer ros_ctrl_shoot_srv_;
 
             ros::ServiceClient gazebo_ros_link_state;
+
             geometry_msgs::Pose gimbal_pose;
             gazebo_msgs::GetLinkState srv;
+            gazebo_msgs::GetLinkState srv_;
 
             std::string gimbal_link;
+            std::string fake_link;
             std::string reference_frame_;
+
+            ignition::math::Quaternion<double> *q_;
 
         /// \brief Maps model IDs to ModelNames
         private: std::map<int, std::string> linkIDToName;
@@ -415,14 +471,19 @@ namespace gazebo
         double reset_frequency = 2.0;
         // Time Memory
         double old_secs;
-        // Force Direction
-        double x_axis_force = 0.0;
-        double y_axis_force = 0.0;
-        double z_axis_force = 0.0;
 
-        // Reseting Flag
-        bool reseting_plugin = false;
-        
+        double force;
+
+        // double gimbal_roll;
+        // double gimbal_pitch;
+        // double gimbal_yaw;
+
+        double gimbal_length = 0.2;
+
+        double x_dir;
+        double y_dir;
+        double z_dir;
+
         int linkIDToName_size = 0;
         int link_to_update_index_now = 0;
         
