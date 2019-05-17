f740ea50d1dee03a1e1f24f53093ffd9619e7d0d
KIM-HC <joel9537@gmail.com>
2019-05-17 22:13:51 +0900

    d

--- icra_roboin_vision/vision_main.cpp
+++ icra_roboin_vision/vision_main.cpp
@@ -16,7 +16,8 @@ int main(int argc, char **argv) {
 	ros::NodeHandle nh;
         ros::NodeHandle n_local("~");
         n_local.param<std::string>("us_color", us_color, "none");
-        ROS_ERROR("us color is : %s\n", us_color);
+        //ROS_ERROR("us color is : %s\n", us_color);
+        ROS_ERROR("us color is : ", us_color, "\n");
 	darknet::Detector detector_yolo(cfg_file, weights_file); // YOLO
 	vision_detector detector(detector_yolo); // object detector class setup
 	send_control send_control; // gimbal에 보낼 정보들
