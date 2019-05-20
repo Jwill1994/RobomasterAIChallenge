c8656c17771b983d823d188985f4696d277f4df6
KIM-HC <joel9537@gmail.com>
2019-05-21 02:30:19 +0900

    d

--- icra_roboin_vision/vision_main.cpp
+++ icra_roboin_vision/vision_main.cpp
@@ -1,19 +1,18 @@
 #include "src/all.hpp"
 
 bool team_col = false;
-bool not_yet = false;
+bool not_yet = true;
 
 void team_color(const roborts_msgs::RobotStatus::ConstPtr& msg) {
         if (team_col == false) {
                 if (msg->id == 3 || msg->id == 4) {us_color = "red";}
                 else if (msg->id == 13 || msg->id == 14) {us_color = "blue";}
-                //ROS_ERROR("our color is : %s\n", us_color.c_str());
                 team_col = true;
         }
 }
 
-void game_starter() {
-        if (not_yet && msg->) {
+void game_starter(const roborts_msgs::GameStatus::ConstPtr& msg) {
+        if (not_yet && msg->game_status == 4) {
                 not_yet = false;
         }
 }
@@ -24,19 +23,15 @@ int main(int argc, char **argv) {
         ros::Subscriber team_sub;
         ros::Subscriber start_sub;
         team_sub = nh.subscribe("robot_status", 2, team_color);
-        start_sub = nh.subscribe("robot_status", 2, game_starter);
+        start_sub = nh.subscribe("game_status", 2, game_starter);
 	darknet::Detector detector_yolo(cfg_file, weights_file); // YOLO
 	vision_detector detector(detector_yolo); // object detector class setup
 	send_control send_control; // gimbal에 보낼 정보들
 	vision_ros vision_ros(nh); // ros부분
 	data_control CT_data; // 데이터 관리
 	ImageConverter imageconverter(&CT_data); // ROS로 이미지 받아오기
-
         while(not_yet) {}
-
 	GimbalControl gimbalControl(nh); // 짐벌조절
-
-
         while (ros::ok()) {
                 ros::spinOnce();
                 if(get_data) {
