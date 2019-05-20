5911e41f3dfb0f9183b9eba1565663cd390293d3
이창민 <hellokiii@naver.com>
2019-05-20 13:19:06 +0900

    gogo

--- icra_roboin_vision/src/GimbalControl.hpp
+++ icra_roboin_vision/src/GimbalControl.hpp
@@ -6,9 +6,12 @@
 #include "icra_roboin_msgs/YawGain.h"
 #include "icra_roboin_msgs/XYOffset.h"
 #include "icra_roboin_msgs/YoloDetectionInfo.h"
+#include "icra_roboin_msgs/ammo.h"
 #include "roborts_msgs/GimbalAngle.h"
 #include "roborts_msgs/ShootCmd.h"
 #include "roborts_msgs/FricWhl.h"
+#include "roborts_msgs/RobotHeat.h"
+#include "roborts_msgs/geometry_msgs.h"
 // #include "obstacle_detector/Obstacles.h" // TODO check again
 #include "ros/ros.h"
 //#include "SendData_Control.hpp"
@@ -20,13 +23,15 @@
 #include <fstream>  // https://ra2kstar.tistory.com/147, https://goscala.tistory.com/7
 #include <random>  // http://www.cplusplus.com/reference/random/normal_distribution/
 
+
 #define X_CENTER_PIXEL 320
 #define Y_CENTER_PIXEL 240
 
 //#define X_CENTER_PIXEL 212
 //#define Y_CENTER_PIXEL 120
 
-#define SEARCH_COUNT 17
+#define SEARCH_COUNT 40
+#define MAX_VEL 1.1
 #define ONE_EXP_LOOP 200
 
 
@@ -36,14 +41,23 @@ class GimbalControl {
 
 private:
     ros::Publisher cmd_gimbal_angle_pub;
+    ros::Subscriber sub_heat;
+    ros::Subscriber sub_cmd_chassis_vel;
+
+    void subHeatCallback(const roborts_msgs::RobotHeat& msg);
+
     ros::ServiceClient cmd_shoot_client;
+    ros::ServiceClient run_fric_client;
+    ros::ServiceClient count_bullet_client;
 
     ros::ServiceServer yaw_server;
     ros::ServiceServer pitch_server;
     ros::ServiceServer offset_server;
+
+
     unsigned int start_time;
-    unsigned ziggletime;
-    float prev_pitch_cmd = -1;
+//    unsigned ziggletime;
+//    float prev_pitch_cmd = -1;
     int cntt = 0;
     float YAW_P_GAIN;
     float YAW_I_GAIN;
@@ -52,10 +66,10 @@ private:
     float PITCH_I_GAIN;
     float PITCH_D_GAIN;
 
-    //float yaw_gains[2][3] = {{0.00045, 0.000006, 0.000203}, {0.0005, 0.0000003, 0.00103}};
-    float yaw_gains[2][3] = {{0.00029, 0.000014, 0.0032}, {0.00022, 0.000013, 0.0035}};  // 기본값 0.0038 0.00001 0.00001
+    //float yaw_gains[2][3] = {{0.00045, 0.000006, 0.000203}, {0.s0005, 0.0000003, 0.00103}};
+    float yaw_gains[2][3] = {{0.00026, 0.000017, 0.0027}, {0.00022, 0.000013, 0.0035}};  // 기본값 0.0038 0.00001 0.00001
 
-    float SEARCH_VEL = 0.032;  // SEARCH_VEL * SEARCH_COUNT = total angle (1.5~1.7)
+    float SEARCH_VEL = 0.04;  // SEARCH_VEL * SEARCH_COUNT = total angle (1.5~1.7)
     bool setCenter = false;
     int search_count = 0;
     short search_dir = 1;
@@ -70,14 +84,17 @@ private:
     roborts_msgs::GimbalAngle gimbal_angle;
     float current_angle = 0.0;  //for sentry
 
-    int x_offset = -5;  //plus -> aim right
+    int x_offset = -20;  //plus -> aim right
     int y_offset = 5;  //minus -> aim up
+    int shoot_count = 0;
     void cmdShoot(int shoot);
     void sentry();
     void calc_offset(float dist);
     unsigned int current_time = 0;
     unsigned int prev_time = 0;
 
+    void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &vel);
+    float chassis_ang_z = 0;
 
     //기동 판단
 //    bool evasion_move = false;
@@ -89,29 +106,12 @@ private:
     void check_movement(send_target_info& target);
     short calculate_diff(short robot_center_x, short robot_box_width, short armor_center_x);
 #define evasion_judge 3
-#define move_thres 0.10
-#define evasion_reset_judge 20
+#define move_thres 0.15  // bigger -> evasion detection low
+#define evasion_reset_judge 5
 
 
 #ifdef SETTING
-/*
-    int loop_count = 0;
-    float min_p = 0;
-    float min_i = 0;
-    float min_d = 0;
-    unsigned int min_cost = 1000000000;
-    unsigned int cost = 0;
 
-    float v_p = 0.00005;
-    float v_i = 0.00005;
-    float v_d = 0.00005;
-
-    default_random_engine generator;
-    float generate_random(float gain, float v);
-    std::normal_distribution<float> random_yaw_p(float yaw_p_gain, float v);
-    std::normal_distribution<float> random_yaw_i(this->YAW_I_GAIN, 0.0001);
-    std::normal_distribution<float> random_yaw_d(this->YAW_D_GAIN, 0.0001);
-*/
     bool SetOffsetCallback(icra_roboin_msgs::XYOffset::Request &req, icra_roboin_msgs::XYOffset::Response &resp);
     bool SetYawGainCallback(icra_roboin_msgs::YawGain::Request &req, icra_roboin_msgs::YawGain::Response &resp);
     bool SetPitchGainCallback(icra_roboin_msgs::PitchGain::Request &req, icra_roboin_msgs::PitchGain::Response &resp);
@@ -123,23 +123,42 @@ public:
     GimbalControl(ros::NodeHandle &nh) {
         cmd_gimbal_angle_pub = nh.advertise<roborts_msgs::GimbalAngle>("cmd_gimbal_angle", 1);
         cmd_shoot_client =  nh.serviceClient<roborts_msgs::ShootCmd>("cmd_shoot");
+        count_bullet_client = nh.serviceClient<icra_roboin_msgs::ammo>("/ammo");
+
+        sub_heat = nh.subscribe("robot_heat", 1000, &GimbalControl::subHeatCallback, this);
+        sub_cmd_chassis_vel = nh.subscribe("cmd_vel", 1, &GimbalControl::cmdVelCallback, this);
 
         // 0.0009 0.0004 0.002 -> good on 0428 morning
         YAW_P_GAIN = yaw_gains[0][0];
         YAW_I_GAIN = yaw_gains[0][1];
         YAW_D_GAIN = yaw_gains[0][2];
         //float yaw_gains[2][3] = {{0.00035, 0.000004, 0.000203}, {0.00025, 0.00000015, 0.0005}};
-        PITCH_P_GAIN = 0.00068;
-        PITCH_I_GAIN = 0.000001;
-        PITCH_D_GAIN = 0.000001;
+        PITCH_P_GAIN = 0.0004;
+        PITCH_I_GAIN = 0.0;
+        PITCH_D_GAIN = 0.0;
         start_time = clock();
+
+        run_fric_client =  nh.serviceClient<roborts_msgs::ShootCmd>("cmd_fric");  // topic name 확인하기
+        roborts_msgs::FricWhl run_fric;
+        run_fric.request.open = true;
+        while(1) {
+            if (run_fric_client.call(shoot_cmd)) {
+                //ROS_INFO("shoot Call Success");
+                if(run_fric_client.response.received) break;
+                //ROS_INFO("shoot call Received");
+            } else {
+                ROS_ERROR("run fric wheel Call Fail");
+            }
+        }
+
+
 #ifdef SETTING
         yaw_server = nh.advertiseService("yaw_gain", &GimbalControl::SetYawGainCallback, this);
         pitch_server = nh.advertiseService("pitch_gain", &GimbalControl::SetPitchGainCallback, this);
         offset_server = nh.advertiseService("xy_offset", &GimbalControl::SetOffsetCallback, this);
 
 #endif
-    
+
     }
 
     void run(send_target_info& target);
@@ -155,16 +174,49 @@ public:
         short robot_box_width = 0;
 */
 
-void GimbalControl::calc_offset(float dist) {
-    if(dist!=dist) dist = 1.5; // 값 정하기
-    x_offset = 0.5*dist - 16.5;
-    y_offset = (int) (30 - 18*dist + 1.56*dist*dist);
-    if(dist < 0.9) y_offset = 50;
-    //printf("y offset: %d\n", y_offset);
+
+void Chassis::ChassisSpeedCtrlCallback(const geometry_msgs::Twist::ConstPtr &vel){
+    roborts_sdk::cmd_chassis_speed chassis_speed;
+    chassis_speed.vx = vel->linear.x*1000;
+    chassis_speed.vy = vel->linear.y*1000;
+    chassis_speed.vw = vel->angular.z * 1800.0 / M_PI;
+    chassis_speed.rotate_x_offset = 0;
+    chassis_speed.rotate_y_offset = 0;
+    chassis_speed_pub_->Publish(chassis_speed);
+}
+
+void GimbalControl::cmdVelCallback(const geometry_msgs::Twist::ConstPtr &vel) {
+    SEARCH_VEL = 0.04 * pow((vel->anglular.z - MAX_VEL),2.0) / 1.21;
+    chassis_ang_z = vel->anglular.z;
 }
 
 
+void GimbalControl::calc_offset(float dist) {
+    if(dist!=dist) dist = 1.5; // 값 정하기
+
+    //y_offset = (int) (30 - 12.18*dist + 0.602*dist*dist);
+    //if(dist < 0.9) y_offset = 30;
+    //printf("y offset: %d\n", y_offset);
+    y_offset = (int) -10*dist - 4;
+    if(dist < 0.9) y_offset = 25;
+}
+
+void GimbalControl::subHeatCallback(const roborts_msgs::RobotHeat &msg) {
+    //printf("heat: %d\n", msg.shooter_heat);
+}
+
 void GimbalControl::sentry() {
+//    if(chassis_ang_z > 0.7) {
+//        gimbal_angle.pitch_mode = false;
+//        gimbal_angle.pitch_angle = 0;
+//        gimbal_angle.yaw_mode = false;
+//        gimbal_angle.yaw_angle = 0;
+//        current_angle = 0;
+//        this->cmd_gimbal_angle_pub.publish(gimbal_angle);
+//        setCenter = true;
+//        return;
+//    }
+
     if(!setCenter)  {
         //printf("set Center\n");
         YAW_P_GAIN = yaw_gains[1][0];
@@ -181,21 +233,28 @@ void GimbalControl::sentry() {
     } else {
         //printf("sentry mode\n");
         gimbal_angle.pitch_mode = false;
-        gimbal_angle.pitch_angle = 0.1;
+        gimbal_angle.pitch_angle = 0;
         gimbal_angle.yaw_mode = false;
         current_angle += SEARCH_VEL * search_dir;
         gimbal_angle.yaw_angle = current_angle;
         this->cmd_gimbal_angle_pub.publish(gimbal_angle);
     }
 
-    if (current_angle >= SEARCH_VEL * SEARCH_COUNT || current_angle <= -SEARCH_VEL * SEARCH_COUNT) {
-        search_dir *= -1;
+    if (current_angle >= SEARCH_VEL * SEARCH_COUNT) {
+        search_dir = -1;
         search_count = 0;
         //printf("--------------direction change----------\n");
     }
     search_count++;
 }
 
+else if (ccurrent_angle <= -1 * SEARCH_VEL * SEARCH_COUNT) {
+search_dir = 1;
+search_count = 0;
+//printf("--------------direction change----------\n");
+}
+search_count++;
+}
 
 
 void GimbalControl::check_movement(send_target_info& target) {
@@ -209,6 +268,7 @@ void GimbalControl::check_movement(send_target_info& target) {
         evasion_reset_count = 0;
         //printf("evasion move?: %d\n", evasion_count);
         if(evasion_count >= evasion_judge) {
+//        if(false) {
             movement_type = 1;
             printf("!!!!evasion move detected!!!! \n");
             this->YAW_P_GAIN = this->yaw_gains[movement_type][0];
@@ -222,7 +282,7 @@ void GimbalControl::check_movement(send_target_info& target) {
         if(evasion_reset_count >= evasion_reset_judge) {
             movement_type = 0;
             evasion_count = 0;
-            printf("reset evasion count\n");
+            // printf("reset evasion count\n");
             this->YAW_P_GAIN = this->yaw_gains[movement_type][0];
             this->YAW_I_GAIN = this->yaw_gains[movement_type][1];
             this->YAW_D_GAIN = this->yaw_gains[movement_type][2];
@@ -246,13 +306,25 @@ void GimbalControl::cmdShoot(int shoot) { // 1: shoot, 0: stop
     roborts_msgs::ShootCmd shoot_cmd;
     shoot_cmd.request.mode = shoot;
     shoot_cmd.request.number = shoot;
+    if(shoot == 1) shoot_count++;
+    if(shoot_count < 4) return;
+    shoot_count = 0;
+
 
     if (cmd_shoot_client.call(shoot_cmd)) {
-//        ROS_INFO("shoot Call Success");
-        if(shoot_cmd.response.received == true){
-//            ROS_INFO("shoot call Received");
+        //ROS_INFO("shoot Call Success");
+        if(shoot_cmd.response.received){
+
+            icra_roboin_msgs::ammo subtract_ammo;
+
+            .reqeust.shoot = true;
+            count_bullet_client.call(subtract_ammo) {
+                ROS_INFO("sent ammo call");
+                printf("remaining bullet: %d\n", subtract_ammo.response.remain_bullet);
+            }
+            //ROS_INFO("shoot call Received");
         } else {
-//            ROS_WARN("shoot call Not Received");
+            //ROS_WARN("shoot call Not Received");
         }
     } else {
         ROS_ERROR("shoot Call Fail");
@@ -265,7 +337,7 @@ void GimbalControl::cmdShoot(int shoot) { // 1: shoot, 0: stop
 
 
 bool GimbalControl::SetOffsetCallback(icra_roboin_msgs::XYOffset::Request &req,
-        icra_roboin_msgs::XYOffset::Response &resp) {
+                                      icra_roboin_msgs::XYOffset::Response &resp) {
 
     if (req.x_offset != 0.0) this->x_offset = req.x_offset;
     if (req.y_offset != 0.0) this->y_offset = req.y_offset;
@@ -278,7 +350,7 @@ bool GimbalControl::SetOffsetCallback(icra_roboin_msgs::XYOffset::Request &req,
 
 
 bool GimbalControl::SetYawGainCallback(icra_roboin_msgs::YawGain::Request &req,
-                        icra_roboin_msgs::YawGain::Response &resp) {
+                                       icra_roboin_msgs::YawGain::Response &resp) {
 
     /*
     if (req.p != 0.0) this->YAW_P_GAIN = req.p;
@@ -305,19 +377,19 @@ bool GimbalControl::SetYawGainCallback(icra_roboin_msgs::YawGain::Request &req,
 }
 
 bool GimbalControl::SetPitchGainCallback(icra_roboin_msgs::PitchGain::Request &req,
-	icra_roboin_msgs::PitchGain::Response &resp) {
+                                         icra_roboin_msgs::PitchGain::Response &resp) {
 
-        if (req.p != 0.0) this->PITCH_P_GAIN = req.p;
-	if (req.i != 0.0) this->PITCH_I_GAIN = req.i;
-	if (req.d != 0.0) this->PITCH_D_GAIN = req.d;
-	printf("pitch pid: %f %f %f\n", this->PITCH_P_GAIN, this->PITCH_I_GAIN, this->PITCH_D_GAIN);
-	yaw_prev_error = 0;
-	pitch_prev_error = 0;
-	yaw_i_error = 0;
-	pitch_i_error = 0;
+    if (req.p != 0.0) this->PITCH_P_GAIN = req.p;
+    if (req.i != 0.0) this->PITCH_I_GAIN = req.i;
+    if (req.d != 0.0) this->PITCH_D_GAIN = req.d;
+    printf("pitch pid: %f %f %f\n", this->PITCH_P_GAIN, this->PITCH_I_GAIN, this->PITCH_D_GAIN);
+    yaw_prev_error = 0;
+    pitch_prev_error = 0;
+    yaw_i_error = 0;
+    pitch_i_error = 0;
 
-        resp.received = true;
-	return true;
+    resp.received = true;
+    return true;
 }
 
 
@@ -337,15 +409,17 @@ void GimbalControl::run(send_target_info& target) {
 
         short pixel_error_x = target.Pixel_X - X_CENTER_PIXEL + x_offset;     //plus -> aim right
         short pixel_error_y = target.Pixel_Y - Y_CENTER_PIXEL + y_offset;     //minus -> aim up
-/*
+
+        /* for woosung
         ofstream outputStream;
         outputStream.open("/home/nvidia/roboinTX2/src/icra_roboin_vision/output.csv", fstream::out | fstream::app);
         outputStream << clock() - start_time << "," << target.robot_center_x << "," << target.robot_center_y << "," << target.robot_distance << "," << target.Pixel_X << "," << target.Pixel_Y << "," << target.distance << endl;
         cout << cntt << " " << clock() - start_time << ", " << target.robot_center_x << ", " << target.robot_center_y << ", " << target.robot_distance << "," << target.Pixel_X << "," << target.Pixel_Y << "," << target.distance << endl;
         outputStream.close();
         cntt++;
-*/
-  //      printf("distance: %f\n", target.distance);
+        */
+
+        //printf("distance: %f\n", target.distance);
         //printf("x error: %d\n", pixel_error_x);
         //printf("y error: %d\n", pixel_error_y);
         //printf("PIXEL_Y: %d, Y_CENTER_PIXEL: %d, y_offset: %d\n", target.Pixel_Y, Y_CENTER_PIXEL, y_offset);
@@ -364,38 +438,32 @@ void GimbalControl::run(send_target_info& target) {
 
         this->current_time = clock() % 10000000000;
 //        printf("current: %d\n", this->current_time);
-	if(this->prev_time == 0) {
-		this->prev_time = this->current_time;
-		return;
-	}
+        if(this->prev_time == 0) {
+            this->prev_time = this->current_time;
+            return;
+        }
         float dt = (this->current_time - this->prev_time) / 10000;
         //printf("dt: %d\n", dt);
-        if(pixel_error_x < X_CENTER_PIXEL * 0.4 && pixel_error_x > -X_CENTER_PIXEL * 0.4) this->yaw_i_error += pixel_error_x * dt;
-        if(pixel_error_y < Y_CENTER_PIXEL * 0.4 && pixel_error_y > -Y_CENTER_PIXEL * 0.4) this->pitch_i_error += pixel_error_y * dt;
+        if(pixel_error_x < X_CENTER_PIXEL * 0.5 && pixel_error_x > -X_CENTER_PIXEL * 0.5) this->yaw_i_error += pixel_error_x * dt;
+        if(pixel_error_y < Y_CENTER_PIXEL * 0.5 && pixel_error_y > -Y_CENTER_PIXEL * 0.5) this->pitch_i_error += pixel_error_y * dt;
+        //this->yaw_i_error += pixel_error_x * dt;
+        //this->pitch_i_error += pixel_error_y * dt;
 
         // pid
         float angle_cmd = -pixel_error_x * this->YAW_P_GAIN - this->yaw_i_error * this->YAW_I_GAIN + (this->yaw_prev_error - pixel_error_x) * this->YAW_D_GAIN / dt;
 
-		//printf("error: %d d_error: %d cmd_angle: %f\n", pixel_error_x, this->yaw_prev_error - pixel_error_x, angle_cmd);
-                //gimbal_angle.pitch_angle = pixel_error_y * PITCH_P_GAIN + pitch_i_error * PITCH_I_GAIN + (pitch_prev_error- pixel_error_y) * PITCH_D_GAIN;
+        //printf("error: %d d_error: %d cmd_angle: %f\n", pixel_error_x, this->yaw_prev_error - pixel_error_x, angle_cmd);
+        //gimbal_angle.pitch_angle = pixel_error_y * PITCH_P_GAIN + pitch_i_error * PITCH_I_GAIN + (pitch_prev_error- pixel_error_y) * PITCH_D_GAIN;
         if (angle_cmd > 0.1) angle_cmd = 0.1;
         else if (angle_cmd < -0.1) angle_cmd = -0.1;
         this->gimbal_angle.yaw_angle = angle_cmd;
 
 
-		//    gimbal_angle.yaw_angle = -pixel_error_x * YAW_P_GAIN;
+        //    gimbal_angle.yaw_angle = -pixel_error_x * YAW_P_GAIN;
         float pitch_cmd = pixel_error_y * this->PITCH_P_GAIN + this->pitch_i_error * this->PITCH_I_GAIN + (this->pitch_prev_error- pixel_error_y) * this->PITCH_D_GAIN / dt;
         if(pitch_cmd > 0.1) pitch_cmd = 0.1;
         else if(pitch_cmd < -0.1) pitch_cmd = -0.1;
         this->gimbal_angle.pitch_angle = pitch_cmd;
-/*
-        if(this->prev_pitch_cmd < 0 && angle_cmd > 0) {
-            unsigned int titi = clock() - ziggletime;
-            printf("ziggletime: %d\n", titi);
-            ziggletime = clock();
-        }
-*/
-        this->prev_pitch_cmd = angle_cmd;
 
 
 
@@ -415,8 +483,8 @@ void GimbalControl::run(send_target_info& target) {
         cmdShoot(0);
 
         this->lost_count++;
-        if(this->lost_count > 10) {
-            this->lost_count = 11;
+        if(this->lost_count > 15) {
+            this->lost_count = 16;
             cmdShoot(0);
 
             this->yaw_i_error = 0;
@@ -433,7 +501,7 @@ void GimbalControl::run(send_target_info& target) {
             }
             */
 
-            this->sentry();
+            //this->sentry();//
 
         }
 
